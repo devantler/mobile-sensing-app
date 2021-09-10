61b59ad0afc5db45a2ba170dae0f78ec603046f8
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2021-09-10 11:10:37 +0200

    Initial Commit

---
+++ MobileSensing/MobileSensingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileSensingApp.swift
+//  MobileSensing
+//
+//  Created by Nikolai Emil Damm on 10/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct MobileSensingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
